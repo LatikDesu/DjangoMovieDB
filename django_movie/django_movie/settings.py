5b216712b186a3952dc7daa33d0ebce285deda92
LatikDesu <evga.abrosimov@gmail.com>
2023-03-15 05:05:09 +0300

    Create html_views

--- django_movie/django_movie/settings.py
+++ django_movie/django_movie/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'movie',
+    'movie.apps.MovieConfig',
 ]
 
 MIDDLEWARE = [
@@ -118,8 +118,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_DIR = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [STATIC_DIR]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- django_movie/movie/views.py
+++ django_movie/movie/views.py
@@ -1,3 +1,11 @@
+from django.views.generic.base import View
 from django.shortcuts import render
+from .models import Movie
 
 # Create your views here.
+
+
+class MovieView(View):
+    def get(self, request):
+        movies = Movie.objects.all()
+        return render(request, 'movie/movies.html', {'movie_list': movies})
