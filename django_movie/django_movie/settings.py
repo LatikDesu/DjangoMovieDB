5001b1c0fcc9463d196651f7e001fd40ac0666c7
LatikDesu <evga.abrosimov@gmail.com>
2023-03-18 09:07:48 +0300

    add VK login

--- django_movie/django_movie/settings.py
+++ django_movie/django_movie/settings.py
@@ -53,6 +53,9 @@ INSTALLED_APPS = [
 
     'allauth',
     'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.vk',
 ]
 
 MIDDLEWARE = [
